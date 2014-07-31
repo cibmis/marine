e28d443a699e8c0555b24cdbc0656653ad5fba48
Bigondis <lee.abner@cibmis.com>
2014-07-30 19:12:50 -0500

    fixed footer

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	
+	<head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Central Illinois Bank</title>
+        <meta name="description" content="Central Illinois Bank provides Personal and Commercial Banking Solutions to the Champaign, Urbana, Bloomington, Peoria, Danville and Monticello areas.">
+        <meta name="keywords" content="Checking, Banking, Commercial, Mobile Banking, Online Banking, Rewards, Interest, Loan, Champaign, Urbana, Bloomington, Peoria, Danville, Monticello">
+        <meta name="viewport" content="width=device-width">
+        <meta name="apple-itunes-app" content="app-id=656267239">
+        
+  <!--#include virtual="header.html" -->
+
+        <main role="main">
+          <div class="container">
+            <p>The page you tried to go to http://www.marinebank.com/index.php has been moved to <a href="http://www.marinebank.com/index.html">http://www.marinebank.com/index.html</a>.  If you got to this page via a bookmarked link, please update your bookmarks.  Thank you.</p>
+          
+          </div>
+        </main>
+
+<!--#include virtual="footer.html" -->
